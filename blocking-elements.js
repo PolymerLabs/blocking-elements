9f964aae04de0e1fe4efb33a47e9186d2a2baee9
valdrinkoshi <valdrin@google.com>
2016-07-26 15:42:51 -0700

    Static private methods. Use Symbols for internal variables

--- blocking-elements.js
+++ blocking-elements.js
@@ -17,6 +17,10 @@
 
 (function(document) {
 
+  /* Symbols for blocking elements and already inert elements */
+  const BLOCKING_ELEMS = Symbol('blockingElements');
+  const ALREADY_INERT_ELEMS = Symbol('alreadyInertElements');
+
   /**
    * `BlockingElements` manages a stack of elements that inert the interaction
    * outside them. The top element is the interactive part of the document.
@@ -30,14 +34,14 @@
        * @type {Array<HTMLElement>}
        * @private
        */
-      this._blockingElements = [];
+      this[BLOCKING_ELEMS] = [];
 
       /**
        * Elements that are already inert before the first blocking element is pushed.
        * @type {Set<HTMLElement>}
        * @private
        */
-      this._alreadyInertElements = new Set();
+      this[ALREADY_INERT_ELEMS] = new Set();
     }
 
     /**
@@ -46,19 +50,20 @@
      */
     destructor() {
       // Loop from the last to first to gradually update the tree up to body.
-      for (let i = this._blockingElements.length - 1; i >= 0; i--) {
-        topChanged(this._blockingElements[i - 1], this._blockingElements[i], this._alreadyInertElements);
+      const elems = this[BLOCKING_ELEMS];
+      for (let i = elems.length - 1; i >= 0; i--) {
+        BlockingElements._topChaged(elems[i - 1], elems[i], this[ALREADY_INERT_ELEMS]);
       }
-      delete this._blockingElements;
-      delete this._alreadyInertElements;
+      delete this[BLOCKING_ELEMS];
+      delete this[ALREADY_INERT_ELEMS];
     }
 
     /**
      * A copy of the blocking elements.
-     * @returns {Array<HTMLElement>}
+     * @type {Array<HTMLElement>}
      */
     get all() {
-      return [...this._blockingElements];
+      return [...this[BLOCKING_ELEMS]];
     }
 
     /**
@@ -66,7 +71,8 @@
      * @type {HTMLElement|null}
      */
     get top() {
-      return this._blockingElements[this._blockingElements.length - 1] || null;
+      const elems = this[BLOCKING_ELEMS];
+      return elems[elems.length - 1] || null;
     }
 
     /**
@@ -74,7 +80,7 @@
      * @param {!HTMLElement} element
      */
     push(element) {
-      const i = this._blockingElements.indexOf(element);
+      const i = this[BLOCKING_ELEMS].indexOf(element);
       // TODO(valdrin) should this element be moved to the top if already in
       // the list?
       if (i !== -1) {
@@ -82,8 +88,8 @@
         return;
       }
       const oldTop = this.top;
-      this._blockingElements.push(element);
-      topChanged(element, oldTop, this._alreadyInertElements);
+      this[BLOCKING_ELEMS].push(element);
+      BlockingElements._topChaged(element, oldTop, this[ALREADY_INERT_ELEMS]);
     }
 
     /**
@@ -91,12 +97,12 @@
      * @param {!HTMLElement} element
      */
     remove(element) {
-      const i = this._blockingElements.indexOf(element);
+      const i = this[BLOCKING_ELEMS].indexOf(element);
       if (i !== -1) {
-        this._blockingElements.splice(i, 1);
+        this[BLOCKING_ELEMS].splice(i, 1);
         // Top changed only if the removed element was the top element.
-        if (i === this._blockingElements.length) {
-          topChanged(this.top, element, this._alreadyInertElements);
+        if (i === this[BLOCKING_ELEMS].length) {
+          BlockingElements._topChaged(this.top, element, this[ALREADY_INERT_ELEMS]);
         }
       }
     }
@@ -110,142 +116,153 @@
       top && this.remove(top);
       return top;
     }
-  }
 
-  /**
-   * Sets `inert` to all document elements except the new top element, its parents,
-   * and its distributed content. Pass `oldTop` to limit element updates (will look
-   * for common parents and avoid setting them twice).
-   * When the first blocking element is added (`newTop = null`), it saves the elements
-   * that are already inert into `alreadyInertElems`. When the last blocking element
-   * is removed (`oldTop = null`), `alreadyInertElems` are kept inert.
-   * @param {HTMLElement} newTop If null, it means the last blocking element was removed.
-   * @param {HTMLElement} oldTop If null, it means the first blocking element was added.
-   * @param {!Set<HTMLElement>} alreadyInertElems Elements to be kept inert.
-   */
-  function topChanged(newTop, oldTop, alreadyInertElems) {
-    const oldElParents = oldTop ? getParents(oldTop) : [];
-    const newElParents = newTop ? getParents(newTop) : [];
-    const elemsToSkip = newTop && newTop.shadowRoot ? getDistributedChildren(newTop.shadowRoot) : null;
-    // Loop from top to deepest elements, so we find the common parents and
-    // avoid setting them twice.
-    while (oldElParents.length || newElParents.length) {
-      const oldElParent = oldElParents.pop();
-      const newElParent = newElParents.pop();
-      if (oldElParent === newElParent) {
-        continue;
-      }
-      // Same parent, set only these 2 children.
-      if (oldElParent && newElParent && oldElParent.parentNode === newElParent.parentNode) {
-        if (!oldTop && oldElParent.inert) {
-          alreadyInertElems.add(oldElParent);
+    /**
+     * Sets `inert` to all document elements except the new top element, its parents,
+     * and its distributed content. Pass `oldTop` to limit element updates (will look
+     * for common parents and avoid setting them twice).
+     * When the first blocking element is added (`newTop = null`), it saves the elements
+     * that are already inert into `alreadyInertElems`. When the last blocking element
+     * is removed (`oldTop = null`), `alreadyInertElems` are kept inert.
+     * @param {HTMLElement} newTop If null, it means the last blocking element was removed.
+     * @param {HTMLElement} oldTop If null, it means the first blocking element was added.
+     * @param {!Set<HTMLElement>} alreadyInertElems Elements to be kept inert.
+     * @private
+     */
+    static _topChaged(newTop, oldTop, alreadyInertElems) {
+      const oldElParents = oldTop ? this._getParents(oldTop) : [];
+      const newElParents = newTop ? this._getParents(newTop) : [];
+      const elemsToSkip = newTop && newTop.shadowRoot ?
+        this._getDistributedChildren(newTop.shadowRoot) : null;
+      // Loop from top to deepest elements, so we find the common parents and
+      // avoid setting them twice.
+      while (oldElParents.length || newElParents.length) {
+        const oldElParent = oldElParents.pop();
+        const newElParent = newElParents.pop();
+        if (oldElParent === newElParent) {
+          continue;
         }
-        oldElParent.inert = true;
-        newElParent.inert = alreadyInertElems.has(newElParent);
-      } else {
-        oldElParent && setInertToSiblingsOfElement(oldElParent, false, elemsToSkip,
-          alreadyInertElems);
-        // Collect the already inert elements only if it is the first blocking element
-        // (if oldTop = null)
-        newElParent && setInertToSiblingsOfElement(newElParent, true, elemsToSkip,
-          oldTop ? null : alreadyInertElems);
-      }
-    }
-    if (!newTop) {
-      alreadyInertElems.clear();
-    }
-  }
-
-  /* Regex for elements that are not inertable. */
-  const NOT_INERTABLE = /^(style|template|script)$/;
-
-  /**
-   * Sets `inert` to the siblings of the element except the elements to skip.
-   * When `inert = true`, already inert elements are added into `alreadyInertElems`;
-   * When `inert = false`, siblings that are contained in `alreadyInertElems` will
-   * be kept inert.
-   * @param {!HTMLElement} element
-   * @param {boolean} inert
-   * @param {Set<HTMLElement>} elemsToSkip
-   * @param {Set<HTMLElement>} alreadyInertElems
-   */
-  function setInertToSiblingsOfElement(element, inert, elemsToSkip, alreadyInertElems) {
-    // Previous siblings.
-    let sibling = element;
-    while ((sibling = sibling.previousElementSibling)) {
-      // If not inertable or to be skipped, skip.
-      if (NOT_INERTABLE.test(sibling.localName) ||
-        (elemsToSkip && elemsToSkip.has(sibling))) {
-        continue;
-      }
-      // Should be collected since already inerted.
-      if (alreadyInertElems && inert && sibling.inert) {
-        alreadyInertElems.add(sibling);
-      }
-      // Should be kept inert if it's in `alreadyInertElems`.
-      sibling.inert = inert || (alreadyInertElems && alreadyInertElems.has(sibling));
-    }
-    // Next siblings.
-    sibling = element;
-    while ((sibling = sibling.nextElementSibling)) {
-      // If not inertable or to be skipped, skip.
-      if (NOT_INERTABLE.test(sibling.localName) ||
-        (elemsToSkip && elemsToSkip.has(sibling))) {
-        continue;
-      }
-      // Should be collected since already inerted.
-      if (alreadyInertElems && inert && sibling.inert) {
-        alreadyInertElems.add(sibling);
-      }
-      // Should be kept inert if it's in `alreadyInertElems`.
-      sibling.inert = inert || (alreadyInertElems && alreadyInertElems.has(sibling));
-    }
-  }
-
-  /**
-   * Returns the list of parents of an element, starting from element (included)
-   * up to `document.body` (excluded).
-   * @param {!HTMLElement} element
-   * @returns {Array<HTMLElement>}
-   */
-  function getParents(element) {
-    const parents = [];
-    let current = element;
-    // Stop to body.
-    while (current && current !== document.body) {
-      let insertionPoints = [];
-      // Skip shadow roots.
-      if (current.nodeType === Node.ELEMENT_NODE) {
-        parents.push(current);
-        // From deepest to top insertion point.
-        insertionPoints = [...current.getDestinationInsertionPoints()];
-      }
-      if (insertionPoints.length) {
-        current = insertionPoints.pop();
-        for (let i = 0; i < insertionPoints.length; i++) {
-          parents.push(insertionPoints[i]);
+        // Same parent, set only these 2 children.
+        if (oldElParent && newElParent &&
+          oldElParent.parentNode === newElParent.parentNode) {
+          if (!oldTop && oldElParent.inert) {
+            alreadyInertElems.add(oldElParent);
+          }
+          oldElParent.inert = true;
+          newElParent.inert = alreadyInertElems.has(newElParent);
+        } else {
+          oldElParent && this._setInertToSiblingsOfElement(oldElParent,
+            false, elemsToSkip, alreadyInertElems);
+          // Collect the already inert elements only if it is the first blocking
+          // element (if oldTop = null)
+          newElParent && this._setInertToSiblingsOfElement(newElParent,
+            true, elemsToSkip, oldTop ? null : alreadyInertElems);
         }
-      } else {
-        current = current.parentNode || current.host;
+      }
+      if (!newTop) {
+        alreadyInertElems.clear();
       }
     }
-    return parents;
-  }
 
-  /**
-   * Returns the distributed children of a shadow root.
-   * @param {!DocumentFragment} shadowRoot
-   * @returns {Set<HTMLElement>}
-   */
-  function getDistributedChildren(shadowRoot) {
-    const result = new Set();
-    // TODO(valdrin) query slots.
-    [...shadowRoot.querySelectorAll('content')].forEach(function(content) {
-      [...content.getDistributedNodes()].forEach(function(child) {
-        (child.nodeType === Node.ELEMENT_NODE) && result.add(child);
+    /**
+     * Returns if the element is not inertable.
+     * @param {!HTMLElement} element
+     * @returns {boolean}
+     * @private
+     */
+    static _notInertable(element) {
+      return /^(style|template|script)$/.test(element.localName);
+    }
+
+    /**
+     * Sets `inert` to the siblings of the element except the elements to skip.
+     * If `inert = true`, already inert elements are added into `alreadyInertElems`.
+     * If `inert = false`, siblings that are contained in `alreadyInertElems` will
+     * be kept inert.
+     * @param {!HTMLElement} element
+     * @param {boolean} inert
+     * @param {Set<HTMLElement>} elemsToSkip
+     * @param {Set<HTMLElement>} alreadyInertElems
+     * @private
+     */
+    static _setInertToSiblingsOfElement(element, inert, elemsToSkip, alreadyInertElems) {
+      // Previous siblings.
+      let sibling = element;
+      while ((sibling = sibling.previousElementSibling)) {
+        // If not inertable or to be skipped, skip.
+        if (this._notInertable(sibling) || (elemsToSkip && elemsToSkip.has(sibling))) {
+          continue;
+        }
+        // Should be collected since already inerted.
+        if (alreadyInertElems && inert && sibling.inert) {
+          alreadyInertElems.add(sibling);
+        }
+        // Should be kept inert if it's in `alreadyInertElems`.
+        sibling.inert = inert || (alreadyInertElems && alreadyInertElems.has(sibling));
+      }
+      // Next siblings.
+      sibling = element;
+      while ((sibling = sibling.nextElementSibling)) {
+        // If not inertable or to be skipped, skip.
+        if (this._notInertable(sibling) || (elemsToSkip && elemsToSkip.has(sibling))) {
+          continue;
+        }
+        // Should be collected since already inerted.
+        if (alreadyInertElems && inert && sibling.inert) {
+          alreadyInertElems.add(sibling);
+        }
+        // Should be kept inert if it's in `alreadyInertElems`.
+        sibling.inert = inert || (alreadyInertElems && alreadyInertElems.has(sibling));
+      }
+    }
+
+    /**
+     * Returns the list of parents of an element, starting from element (included)
+     * up to `document.body` (excluded).
+     * @param {!HTMLElement} element
+     * @returns {Array<HTMLElement>}
+     * @private
+     */
+    static _getParents(element) {
+      const parents = [];
+      let current = element;
+      // Stop to body.
+      while (current && current !== document.body) {
+        let insertionPoints = [];
+        // Skip shadow roots.
+        if (current.nodeType === Node.ELEMENT_NODE) {
+          parents.push(current);
+          // From deepest to top insertion point.
+          insertionPoints = [...current.getDestinationInsertionPoints()];
+        }
+        if (insertionPoints.length) {
+          current = insertionPoints.pop();
+          for (let i = 0; i < insertionPoints.length; i++) {
+            parents.push(insertionPoints[i]);
+          }
+        } else {
+          current = current.parentNode || current.host;
+        }
+      }
+      return parents;
+    }
+
+    /**
+     * Returns the distributed children of a shadow root.
+     * @param {!DocumentFragment} shadowRoot
+     * @returns {Set<HTMLElement>}
+     * @private
+     */
+    static _getDistributedChildren(shadowRoot) {
+      const result = new Set();
+      // TODO(valdrin) query slots.
+      [...shadowRoot.querySelectorAll('content')].forEach(function(content) {
+        [...content.getDistributedNodes()].forEach(function(child) {
+          (child.nodeType === Node.ELEMENT_NODE) && result.add(child);
+        });
       });
-    });
-    return result;
+      return result;
+    }
   }
 
   document.$blockingElements = new BlockingElements();
